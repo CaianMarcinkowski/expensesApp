0e5dcaf36318adba06562da3ffc14cb4d351d478
Caian Marcinkowski Ferreira <caianferre@gmail.com>
2024-09-25 11:12:57 -0300

    Initial commit

---
+++ lib/components/transaction_list.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import './transaction_item.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final void Function(String) onRemove;
+
+  const TransactionList(this.transactions, this.onRemove, {Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return transactions.isEmpty
+        ? LayoutBuilder(
+            builder: (ctx, constraints) {
+              return Column(
+                children: [
+                  const SizedBox(height: 20),
+                  Text(
+                    'Nenhuma Transação Cadastrada!',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
+              );
+            },
+          )
+        :
+        // ListView(
+        //     children: transactions.map((tr) {
+        //       return TransactionItem(
+        //         key: ValueKey(tr.id),
+        //         tr: tr,
+        //         onRemove: onRemove,
+        //       );
+        //     }).toList(),
+        //   );
+        ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: (ctx, index) {
+              final tr = transactions[index];
+              return TransactionItem(
+                key: GlobalObjectKey(tr),
+                tr: tr,
+                onRemove: onRemove,
+              );
+            },
+          );
+  }
+}
